913d3dcaaa813311edab6ea5c5d9e23fd6bfa067
kasule joseph <joseph.kasule@andela.com>
2019-11-13 15:32:10 +0200

    filter districts

--- ug_dst/main.py
+++ ug_dst/main.py
@@ -7,12 +7,14 @@ import argparse
 import json
 import os
 import requests
+import operator
 
 
 def main():
     dist_url = get_data()
 
     read_file(dist_url.path)
+    district(dist_url.path)
 
 
 def get_data():
@@ -23,10 +25,24 @@ def get_data():
 def read_file(get_data):
     response = requests.get(get_data)
 
-    print(get_data)
-    print(response.json()[0])
+    # print(get_data)
+    # print(response.json()[0])
+    sorted_x = sorted(response.json(), key=operator.itemgetter("district_name"), reverse=False)
+    # print(sorted_x[0])
 
+def district(get_data):
+    response = requests.get(get_data)
+    sorted_x = sorted(response.json(), key=operator.itemgetter("district_name"), reverse=False)
+    districts = dict()
+    districtsArray = []
 
+    for value in sorted_x:
+        if(value['district_name'] not in districts.values()):
+            district_name = districts['district_name'] = value['district_name']
+            district_code = districts['district_code'] = value['district_code']
+            province_name = districts['province_name'] = value['province_name']
+            districtsArray.append({'district_name': district_name, 'district_code': district_code, 'province_name': province_name})
+    return districtsArray       
 
 if __name__ == "__main__":
     main()
